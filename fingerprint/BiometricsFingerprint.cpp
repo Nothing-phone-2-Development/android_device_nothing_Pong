58d941101e961b4bbecc7eae5035d13bb3284db8
Demon000 <demonsingur@gmail.com>
2020-05-29 00:03:15 +0200

    Pong: fingerprint: Report finger events on sysfs notification
    Change-Id: I78069d10677c6704b0bb080ecf76ecbbfd0b2212
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -22,7 +22,35 @@
 #include "BiometricsFingerprint.h"
 
 #include <inttypes.h>
+#include <poll.h>
 #include <unistd.h>
+
+#include <thread>
+
+#define FOD_UI_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui"
+
+namespace {
+
+static bool readBool(int fd) {
+    char c;
+    int rc;
+
+    rc = lseek(fd, 0, SEEK_SET);
+    if (rc) {
+        ALOGE("failed to seek fd, err: %d", rc);
+        return false;
+    }
+
+    rc = read(fd, &c, sizeof(char));
+    if (rc != 1) {
+        ALOGE("failed to read bool from fd, err: %d", rc);
+        return false;
+    }
+
+    return c != '0';
+}
+
+}  // anonymous namespace
 
 namespace android {
 namespace hardware {
@@ -44,6 +72,30 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
     if (!mDevice) {
         ALOGE("Can't open HAL module");
     }
+
+    std::thread([this]() {
+        int fd = open(FOD_UI_PATH, O_RDONLY);
+        if (fd < 0) {
+            ALOGE("failed to open fd, err: %d", fd);
+            return;
+        }
+
+        struct pollfd fodUiPoll = {
+                .fd = fd,
+                .events = POLLERR | POLLPRI,
+                .revents = 0,
+        };
+
+        while (true) {
+            int rc = poll(&fodUiPoll, 1, -1);
+            if (rc < 0) {
+                ALOGE("failed to poll fd, err: %d", rc);
+                continue;
+            }
+
+            mDevice->goodixExtCmd(mDevice, readBool(fd) ? 1 : 0, 0);
+        }
+    }).detach();
 }
 
 BiometricsFingerprint::~BiometricsFingerprint() {
@@ -352,12 +404,10 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t /* sensorId */) {
 
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y */,
                                                  float /* minor */, float /* major */) {
-    mDevice->goodixExtCmd(mDevice, 1, 0);
     return Void();
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
-    mDevice->goodixExtCmd(mDevice, 0, 0);
     return Void();
 }
 
