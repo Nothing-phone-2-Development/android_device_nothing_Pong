c1729d855e655c6576e1d80f817f253a04e7354b
LuK1337 <priv.luk@gmail.com>
2021-08-02 22:47:55 +0200

    Pong: Include device FCM from vendor/lineage
    Sadly we aren't able to include it globally without breaking other
    devices ;((((
    
    Change-Id: Ia29d6fb047a09a2b4767312cb2a3252c87095723
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,6 +77,10 @@ TARGET_SCREEN_DENSITY := 420
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    vendor/lineage/config/device_framework_matrix.xml
+
 # Kernel
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
