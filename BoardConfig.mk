b8796cc33255fe29cd2680e01a3695e1698219c3
Bruno Martins <bgcngm@gmail.com>
2022-04-05 10:34:38 +0100

    Pong: Inherit QTI FCM
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,6 +80,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 # Kernel
