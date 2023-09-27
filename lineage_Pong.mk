ea39323b9631d17e19ae5550d36993fe07342cc2
chandu078 <chandudyavanapelli03@gmail.com>
2023-09-27 17:16:00 +0530

    Pong: Initial product definition for Nothing phone (2)
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

---
+++ lineage_Pong.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from Pong device
+$(call inherit-product, device/nothing/Pong/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_Pong
+PRODUCT_DEVICE := Pong
+PRODUCT_MANUFACTURER := nothing
+PRODUCT_BRAND := Nothing
+PRODUCT_MODEL := A065
+
+PRODUCT_SYSTEM_NAME := Pong
+PRODUCT_SYSTEM_DEVICE := Pong
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_GMS_CLIENTID_BASE := android-nothing
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="Pong-user 12 SKQ1.221207.001 2309061933 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := Nothing/Pong/Pong:12/SKQ1.221207.001/2309061933:user/release-keys
