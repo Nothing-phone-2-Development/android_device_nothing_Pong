1b615afd3decbb0dfff652aee0b614e300247c63
chandu078 <chandudyavanapelli03@gmail.com>
2023-10-28 08:46:53 +0200

    Pong: Patch libwfdnative
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        system_ext/lib64/libwfdnative.so)
+            sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
